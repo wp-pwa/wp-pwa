f7573c7f39edf35b6db0a6ea6a5ed516a5b7b7e1
Luis Herranz <luisherranz@gmail.com>
2017-10-20 17:07:03 +0200

    Add env variables (WIP)

---
+++ core/pwa/scripts/start.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console, global-require */
+const { emptyDir } = require('fs-extra');
+const express = require('express');
+const webpack = require('webpack');
+const noFavicon = require('express-no-favicons');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackHotServerMiddleware = require('webpack-hot-server-middleware');
+const clientConfig = require('./webpack/client.dev');
+const serverConfig = require('./webpack/server.dev');
+const clientConfigProd = require('./webpack/client.prod');
+const serverConfigProd = require('./webpack/server.prod');
+
+const { publicPath, path: outputPath } = clientConfig.output;
+
+const DEV = process.env.NODE_ENV === 'development';
+
+const start = async () => {
+  await emptyDir('.build/pwa');
+
+  const app = express();
+  app.use(noFavicon());
+
+  let isBuilt = false;
+
+  const done = () =>
+    !isBuilt &&
+    app.listen(3000, () => {
+      isBuilt = true;
+      console.log('BUILD COMPLETE -- Listening @ http://localhost:3000');
+    });
+
+  if (DEV) {
+    const compiler = webpack([clientConfig, serverConfig]);
+    const clientCompiler = compiler.compilers[0];
+    const options = { publicPath, stats: { colors: true } };
+
+    app.use(webpackDevMiddleware(compiler, options));
+    app.use(webpackHotMiddleware(clientCompiler));
+    app.use(webpackHotServerMiddleware(compiler));
+
+    compiler.plugin('done', done);
+  } else {
+    webpack([clientConfigProd, serverConfigProd]).run((err, stats) => {
+      const clientStats = stats.toJson().children[0];
+      const serverRender = require('../../../.build/pwa/server/main.js').default; // eslint-disable-line
+
+      app.use(publicPath, express.static(outputPath));
+      app.use(serverRender({ clientStats }));
+
+      done();
+    });
+  }
+};
+
+start();
