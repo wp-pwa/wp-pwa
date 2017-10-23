2855ffb3bbf50043a0d38a5a9eaa16c61e37242b
Luis Herranz <luisherranz@gmail.com>
2017-10-23 12:05:38 +0200

    Add HMR path support

--- core/pwa/scripts/webpack/client.dev.js
+++ core/pwa/scripts/webpack/client.dev.js
@@ -1,14 +1,15 @@
-const path = require('path')
-const webpack = require('webpack')
-const WriteFilePlugin = require('write-file-webpack-plugin') // here so you can see what chunks are built
-const ExtractCssChunks = require('extract-css-chunks-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const WriteFilePlugin = require('write-file-webpack-plugin'); // here so you can see what chunks are built
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
 
 module.exports = {
   name: 'client',
   target: 'web',
   // devtool: 'eval',
   entry: [
-    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false&quiet=false&noInfo=false',
+    `webpack-hot-middleware/client?path=${process.env.HMR_PATH ||
+      '/'}__webpack_hmr&timeout=20000&reload=false&quiet=false&noInfo=false`,
     'react-hot-loader/patch',
     path.resolve(__dirname, '../../init/public-path.js'),
     path.resolve(__dirname, '../../init/client.js'),
@@ -42,10 +43,10 @@ module.exports = {
               },
             },
           ],
-          publicPath: '/static/',
-        })
-      }
-    ]
+          publicPath: `${process.env.HMR_PATH || '/'}static/`,
+        }),
+      },
+    ],
   },
   plugins: [
     new WriteFilePlugin(),
@@ -53,15 +54,14 @@ module.exports = {
     new webpack.optimize.CommonsChunkPlugin({
       names: ['bootstrap'], // needed to put webpack bootstrap code before chunks
       filename: '[name].js',
-      minChunks: Infinity
+      minChunks: Infinity,
     }),
-
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('development')
-      }
-    })
-  ]
-}
+        NODE_ENV: JSON.stringify('development'),
+      },
+    }),
+  ],
+};
