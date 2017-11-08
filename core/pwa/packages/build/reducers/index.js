57fa7b3dda902e6a94ed4afabfbde255039d36e5
Luis Herranz <luisherranz@gmail.com>
2017-11-08 13:12:37 +0100

    Add jest

--- core/pwa/packages/build/reducers/index.js
+++ core/pwa/packages/build/reducers/index.js
@@ -1,28 +1,28 @@
 import { combineReducers } from 'redux';
 import * as types from '../types';
 
-const packages = (state = {}, action) => {
+export const packages = (state = {}, action) => {
   if (action.type === types.BUILD_UPDATED && action.packages)
     return action.packages;
   return state;
 }
 
-const siteId = (state = null, action) => {
+export const siteId = (state = null, action) => {
   if (action.type === types.BUILD_UPDATED && action.siteId) return action.siteId;
   return state;
 };
 
-const environment = (state = 'pre', action) => {
+export const environment = (state = 'pre', action) => {
   if (action.type === types.BUILD_UPDATED && action.environment) return action.environment;
   return state;
 };
 
-const amp = (state = false, action) => {
+export const amp = (state = false, action) => {
   if (action.type === types.BUILD_UPDATED && action.amp) return action.amp;
   return state;
 };
 
-const ssr = (state = true, action) => {
+export const ssr = (state = true, action) => {
   if (action.type === types.CLIENT_RENDERED) return false;
   return state;
 };
