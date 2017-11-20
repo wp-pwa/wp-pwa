21bd58dd9f15bab6307decbe5a36de63a63b219f
Luis Herranz <luisherranz@gmail.com>
2017-11-20 13:42:07 +0100

    Prettify watch.js

--- watch.js
+++ watch.js
@@ -2,13 +2,13 @@
 const watch = require('watch');
 
 watch.watchTree('.', (f, curr, prev) => {
-    if (typeof f === "object" && prev === null && curr === null) {
-      // Finished walking the tree
-    } else if (prev === null) {
-      console.log(`created: ${f}`);
-    } else if (curr.nlink === 0) {
-      console.log(`removed: ${f}`);
-    } else {
-      console.log(`changed: ${f}`);
-    }
-  })
+  if (typeof f === 'object' && prev === null && curr === null) {
+    // Finished walking the tree
+  } else if (prev === null) {
+    console.log(`created: ${f}`);
+  } else if (curr.nlink === 0) {
+    console.log(`removed: ${f}`);
+  } else {
+    console.log(`changed: ${f}`);
+  }
+});
