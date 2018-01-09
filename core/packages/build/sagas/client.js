6beae2ad3ced29e733a175f1359da22339c37170
David Arenas <darensr@gmail.com>
2018-01-09 15:21:47 +0100

    Moved gtm settings to theme

--- core/packages/build/sagas/client.js
+++ core/packages/build/sagas/client.js
@@ -75,8 +75,11 @@ export const succeedHandlerCreator = ({ connection }) =>
 
 export default function* gtmSagas({ stores }) {
   // Do not execute saga if analytics is disabled for this client
-  const gtm = yield select(getSetting('generalSite', 'gtm'));
-  if (gtm && gtm.analytics && gtm.analytics.disabled) return;
+  const gtm = yield select(getSetting('theme', 'gtm'));
+  if (gtm && gtm.analytics && gtm.analytics.disabled) {
+    // console.log('ANALYTICS DISABLED');
+    return;
+  }
 
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
