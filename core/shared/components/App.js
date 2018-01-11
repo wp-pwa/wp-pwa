dba17bd3e5195b024bf72cbcb18940b69678961c
Eduardo Campaña <asrielo@gmail.com>
2018-01-11 11:38:33 +0100

    Changes div for Fragment on App

--- core/shared/components/App.js
+++ core/shared/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Provider as ReduxProvider } from 'react-redux';
@@ -6,24 +6,24 @@ import { Provider as MobxProvider } from 'mobx-react';
 import GoogleTagManager from './GoogleTagManager';
 import Universal from './Universal';
 
-
 const App = ({ packages, store, stores }) => (
   <ReduxProvider store={store}>
     <MobxProvider {...stores}>
-      <div>
+      <Fragment>
         <Helmet>
           <title>WP PWA</title>
         </Helmet>
-        <GoogleTagManager gtmId="GTM-K3S2BMT"/>
+        <GoogleTagManager gtmId="GTM-K3S2BMT" />
         {packages.map(name => <Universal key={name} name={name} />)}
-      </div>
+      </Fragment>
     </MobxProvider>
   </ReduxProvider>
 );
+
 App.propTypes = {
   packages: PropTypes.arrayOf(PropTypes.string).isRequired,
   store: PropTypes.shape().isRequired,
-  stores: PropTypes.shape().isRequired,
+  stores: PropTypes.shape().isRequired
 };
 
 export default App;
