ed22ed0a057ddf1e7e216d2c43d6302dff18a1ee
Eduardo Campaña <asrielo@gmail.com>
2018-01-16 12:02:38 +0100

    Removes GTM on AMP

--- core/shared/components/GoogleTagManager.js
+++ core/shared/components/GoogleTagManager.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet'
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
-export const gtmScript = (gtmId) => (
+export const gtmScript = gtmId => (
   <script async src={`https://www.googletagmanager.com/gtm.js?id=${gtmId}`} />
 );
 
-export const gtmNoScript = (gtmId) => (
+export const gtmNoScript = gtmId => (
   <noscript>
     {`<iframe
       src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
@@ -17,14 +18,24 @@ export const gtmNoScript = (gtmId) => (
   </noscript>
 );
 
-const GoogleTagManager = ({ gtmId }) => (
-  <Helmet>
-    {gtmScript(gtmId)}
-    {gtmNoScript(gtmId)}
-  </Helmet>
-);
+const GoogleTagManager = ({ gtmId, isAmp }) => {
+  if (isAmp) return null;
+
+  return (
+    <Helmet>
+      {gtmScript(gtmId)}
+      {gtmNoScript(gtmId)}
+    </Helmet>
+  );
+};
+
 GoogleTagManager.propTypes = {
   gtmId: PropTypes.string.isRequired,
-}
+  isAmp: PropTypes.bool.isRequired
+};
 
-export default GoogleTagManager;
+const mapStateToProps = state => ({
+  isAmp: state.build.amp
+});
+
+export default connect(mapStateToProps)(GoogleTagManager);
