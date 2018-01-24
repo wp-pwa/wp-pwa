6076be4157f6af61dabc874503af4fe9275db3cd
Luis Herranz <luisherranz@gmail.com>
2018-01-24 11:56:37 +0100

    Add generator meta

--- core/server/pwa-template.js
+++ core/server/pwa-template.js
@@ -16,6 +16,7 @@ export default ({
   <html ${helmet.htmlAttributes.toString()}>
     <head>
       <meta charset="utf-8">
+      <meta name="generator" content="WordPress PWA">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
       ${styles}
       ${preloadScripts}
