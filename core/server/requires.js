dcca3ec32507a18a4429f9cbdd38256f2ebb5060
Luis Herranz <luisherranz@gmail.com>
2018-01-25 13:32:53 +0100

    Fix problem with Webpack chunks
    Fixes #19

--- core/server/requires.js
+++ core/server/requires.js
@@ -14,10 +14,10 @@ export const requireModules = async pkgs => {
   const pathPromises = pkgs.map(([, name]) => pathExistsPromise(name));
   await Promise.all(pathPromises);
   return pkgs.map(([namespace, name]) => {
-    const module = require(`../../packages/${name}/src/${process.env.MODE}/index.js`);
+    const module = require(`../../packages/${name}/src/${process.env.MODE}/index`);
     try {
       // Only return serverSaga if it exists.
-      const serverSaga = require(`../../packages/${name}/src/${process.env.MODE}/sagas/server.js`)
+      const serverSaga = require(`../../packages/${name}/src/${process.env.MODE}/sagas/server`)
         .default;
       return { name, namespace, module, serverSaga };
     } catch (e) {
