f19237f09f4fcf5d7e0eb17588af59dd1e4b4cd0
David Arenas <darensr@gmail.com>
2018-02-23 12:09:45 +0100

    Fixes comScore component

--- core/shared/components/ComScore.js
+++ core/shared/components/ComScore.js
@@ -1,24 +1,29 @@
-import React from 'react';
+/* eslint-disable react/no-danger */
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 
 export const comScoreScript = id => (
-  <script async type="application/javascript">
-    {`
-    var _comscore = _comscore || [];
+  <script
+    async
+    type="application/javascript"
+    dangerouslySetInnerHTML={{
+      __html: `
+var _comscore = _comscore || [];
 
-    var pixelComscore = function() {
-        _comscore.push({ c1: "2", c2: "${id}" });
-        (function() {
-            var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
-            s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
-            el.parentNode.insertBefore(s, el);
-        })();
-    };
+var pixelComscore = function() {
+    _comscore.push({ c1: "2", c2: "${id}" });
+    (function() {
+        var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
+        s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
+        el.parentNode.insertBefore(s, el);
+    })();
+};
 
-    pixelComscore();
-  `}
-  </script>
+pixelComscore();
+`,
+    }}
+  />
 );
 
 export const comScoreNoScript = id => (
@@ -28,10 +33,10 @@ export const comScoreNoScript = id => (
 );
 
 const ComScore = ({ id }) => (
-  <Helmet>
+  <Fragment>
+    <Helmet>{comScoreNoScript(id)}</Helmet>
     {comScoreScript(id)}
-    {comScoreNoScript(id)}
-  </Helmet>
+  </Fragment>
 );
 
 ComScore.propTypes = {
