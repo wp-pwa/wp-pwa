c278b9fd8699af40161048f6b2d7e6462a371c33
Eduardo Campaña <asrielo@gmail.com>
2018-03-01 15:46:43 +0100

    Moves analytics sagas to analytics package in core #55

--- core/packages/analytics/shared/sagas/client.js
+++ core/packages/analytics/shared/sagas/client.js
@@ -1,7 +1,8 @@
 import { fork, all } from 'redux-saga/effects';
 import gtmSagas from './gtm';
 import comScoreSagas from './comScore';
+import analytics from './analytics';
 
 export default function* buildClientSagas({ stores }) {
-  yield all([fork(gtmSagas, stores), fork(comScoreSagas, stores)]);
+  yield all([fork(gtmSagas, stores), fork(comScoreSagas, stores)], fork(analytics, stores));
 }
