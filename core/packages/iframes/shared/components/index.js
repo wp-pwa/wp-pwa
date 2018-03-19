c9a1eb35e8383e6193441e0111686bd36b1ef429
David Arenas <darensr@gmail.com>
2018-03-19 14:58:12 +0100

    Iframe with animation on load

--- core/packages/iframes/shared/components/index.js
+++ core/packages/iframes/shared/components/index.js
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Fill } from 'react-slot-fill';
+import { dep } from 'worona-deps';
+import Iframe from './iframe';
 import { getIframesForMobile } from '../selectors';
 
-const Iframes = ({ iframes }) =>
+const Iframes = ({ iframes, ssr }) =>
   iframes.map(({ name, src, className, width, height }) => (
     <Fill key={name} name={name}>
-      <iframe
+      <Iframe
         title={name}
         src={src}
         className={className}
         width={width}
         height={height}
+        ssr={ssr}
         style={{
           margin: '0 auto',
           display: 'block',
@@ -32,10 +35,12 @@ Iframes.propTypes = {
       height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     }),
   ),
+  ssr: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   iframes: getIframesForMobile(state),
+  ssr: dep('build', 'selectors', 'getSsr')(state),
 });
 
 export default connect(mapStateToProps)(Iframes);
