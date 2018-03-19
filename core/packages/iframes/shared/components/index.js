e4ba8bf871abf8cee798f4fe8a43d9ca74402a80
David Arenas <darensr@gmail.com>
2018-03-19 17:52:14 +0100

    Removes logic from fill

--- core/packages/iframes/shared/components/index.js
+++ core/packages/iframes/shared/components/index.js
@@ -2,20 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Fill } from 'react-slot-fill';
-import { dep } from 'worona-deps';
-import Iframe from './iframe';
 import { getIframesForMobile } from '../selectors';
 
-const Iframes = ({ iframes, ssr }) =>
+const Iframes = ({ iframes }) =>
   iframes.map(({ name, src, className, width, height }) => (
     <Fill key={name} name={name}>
-      <Iframe
+      <iframe
         title={name}
         src={src}
         className={className}
         width={width}
         height={height}
-        ssr={ssr}
         style={{
           margin: '0 auto',
           display: 'block',
@@ -35,12 +32,10 @@ Iframes.propTypes = {
       height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
     }),
   ),
-  ssr: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   iframes: getIframesForMobile(state),
-  ssr: dep('build', 'selectors', 'getSsr')(state),
 });
 
 export default connect(mapStateToProps)(Iframes);
