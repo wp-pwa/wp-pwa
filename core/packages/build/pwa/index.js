3855d9cc35e316e45332102fd4a8cfebb69336fa
Luis Herranz <luisherranz@gmail.com>
2018-05-04 16:27:48 +0200

    Add version to JS

--- core/packages/build/pwa/index.js
+++ core/packages/build/pwa/index.js
@@ -3,8 +3,9 @@ import * as actionTypes from '../shared/actionTypes';
 import reducers from '../shared/reducers';
 import * as selectors from '../shared/selectors';
 import Store from '../shared/stores';
+import { version } from '../../../../package.json';
 
 const Build = () => null;
 
 export default Build;
-export { actions, actionTypes, reducers, selectors, Store };
+export { actions, actionTypes, reducers, selectors, Store, version };
