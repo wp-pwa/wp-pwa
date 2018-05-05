9ae863c816fc9aba4f5b58efc8f1a9e811a37cb5
Eduardo Campaña <asrielo@gmail.com>
2018-05-06 00:38:43 +0200

    Creates an empty MST store in settings
    #79

--- core/packages/settings/amp/index.js
+++ core/packages/settings/amp/index.js
@@ -3,8 +3,9 @@ import * as actionTypes from '../shared/actionTypes';
 import reducers from '../shared/reducers';
 import * as selectors from '../shared/selectors';
 import * as selectorCreators from '../shared/selectorCreators';
+import Store from '../shared/stores';
 
 const Settings = () => null;
 
 export default Settings;
-export { actions, actionTypes, reducers, selectors, selectorCreators };
+export { actions, actionTypes, reducers, selectors, selectorCreators, Store };
