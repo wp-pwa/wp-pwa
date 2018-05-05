a4f615deb3af87c04cdb97b203b6dc0872c3f616
Eduardo Campaña <asrielo@gmail.com>
2018-05-06 01:31:30 +0200

    Adds settings to the MST store
    #79

--- core/packages/settings/shared/stores/index.js
+++ core/packages/settings/shared/stores/index.js
@@ -1,3 +1,29 @@
-import { types } from 'mobx-state-tree';
+import { types, getEnv } from 'mobx-state-tree';
+import * as actionTypes from '../actionTypes';
 
-export default types.model('Settings').props({});
+export default types
+  .model('Settings')
+  .props({
+    modules: types.optional(types.frozen, {}),
+  })
+  .actions(self => {
+    const { store, isClient } = getEnv(self);
+
+    return {
+      [actionTypes.SETTINGS_UPDATED]({ settings }) {
+        self.modules = settings;
+      },
+      afterCreate: () => {
+        if (!isClient) {
+          if (store)
+            store.subscribe(() => {
+              const action = store.getState().lastAction;
+
+              if (self[action.type]) {
+                self[action.type](action);
+              }
+            });
+        }
+      },
+    };
+  });
