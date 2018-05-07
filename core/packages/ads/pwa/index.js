023d6746da1bab5749909023e7926430c1f1fb59
David Arenas <darensr@gmail.com>
2018-05-07 17:03:29 +0200

    Fixes the export of reducers in ads package

--- core/packages/ads/pwa/index.js
+++ core/packages/ads/pwa/index.js
@@ -1,9 +1,9 @@
 import AdsFills, * as components from '../shared/components';
 import * as actions from '../shared/actions';
 import * as actionTypes from '../shared/actionTypes';
-import * as reducers from '../shared/reducers';
 import * as selectors from '../shared/selectors';
 import * as selectorCreators from '../shared/selectorCreators';
+import reducers from '../shared/reducers';
 import Store from '../shared/stores';
 
 export default AdsFills;
