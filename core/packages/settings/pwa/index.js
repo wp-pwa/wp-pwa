d5cead11b78dafa1b08feb1f7312604194b7b521
Eduardo Campaña <asrielo@gmail.com>
2018-05-07 07:39:32 +0200

    Remove selectors imports in amp and pwa from settings
    #79

--- core/packages/settings/pwa/index.js
+++ core/packages/settings/pwa/index.js
@@ -1,11 +1,10 @@
 import * as actions from '../shared/actions';
 import * as actionTypes from '../shared/actionTypes';
 import reducers from '../shared/reducers';
-import * as selectors from '../shared/selectors';
 import * as selectorCreators from '../shared/selectorCreators';
 import Store from '../shared/stores';
 
 const Settings = () => null;
 
 export default Settings;
-export { actions, actionTypes, reducers, selectors, selectorCreators, Store };
+export { actions, actionTypes, reducers, selectorCreators, Store };
