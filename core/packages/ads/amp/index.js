2d2710566443d420bc02b9fe05b1c2200b23cc3b
David Arenas <darensr@gmail.com>
2018-05-07 13:40:57 +0200

    Exports stores

--- core/packages/ads/amp/index.js
+++ core/packages/ads/amp/index.js
@@ -4,6 +4,7 @@ import * as actionTypes from '../shared/actionTypes';
 import * as reducers from '../shared/reducers';
 import * as selectors from '../shared/selectors';
 import * as selectorCreators from '../shared/selectorCreators';
+import Store from '../shared/stores';
 
 export default AdsFills;
-export { actions, actionTypes, reducers, selectors, selectorCreators, components };
+export { actions, actionTypes, reducers, selectors, selectorCreators, components, Store };
