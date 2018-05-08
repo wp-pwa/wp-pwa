73b5132dc0ec0d2d84871d92253e5e4e79aa2f4d
Eduardo Campaña <asrielo@gmail.com>
2018-05-08 17:18:35 +0200

    Fixes ads reducers export in AMP

--- core/packages/ads/amp/index.js
+++ core/packages/ads/amp/index.js
@@ -1,7 +1,7 @@
 import AdsFills, * as components from '../shared/components';
 import * as actions from '../shared/actions';
 import * as actionTypes from '../shared/actionTypes';
-import * as reducers from '../shared/reducers';
+import reducers from '../shared/reducers';
 import * as selectors from '../shared/selectors';
 import * as selectorCreators from '../shared/selectorCreators';
 
