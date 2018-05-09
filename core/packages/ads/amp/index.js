3a064e5cd12c4edd3c52565902a70878b2e43b35
David Arenas <darensr@gmail.com>
2018-05-09 21:02:14 +0200

    WIP removing reducers and selectors from ads package

--- core/packages/ads/amp/index.js
+++ core/packages/ads/amp/index.js
@@ -1,10 +1,7 @@
 import AdsFills, * as components from '../shared/components';
 import * as actions from '../shared/actions';
 import * as actionTypes from '../shared/actionTypes';
-import * as selectors from '../shared/selectors';
-import * as selectorCreators from '../shared/selectorCreators';
-import reducers from '../shared/reducers';
 import Store from '../shared/stores';
 
 export default AdsFills;
-export { actions, actionTypes, reducers, selectors, selectorCreators, components, Store };
+export { actions, actionTypes, components, Store };
--- core/packages/ads/shared/stores/index.js
+++ core/packages/ads/shared/stores/index.js
@@ -1,8 +1,50 @@
-import { types } from 'mobx-state-tree';
+import { types, getParent } from 'mobx-state-tree';
 import Sticky from './sticky';
 
 export default types
   .model('Ads')
   .props({
     sticky: types.optional(Sticky, {}),
-  });
+  })
+  .views(self => ({
+    get root() {
+      return getParent(self);
+    },
+    get doesStickyExist() {
+      const config = self.root.settings.ads;
+      return (
+        !!config &&
+        !!config.formats &&
+        config.formats.filter(({ options }) => options && options.sticky && options.sticky.display)
+          .length > 0
+      );
+    },
+    getFormats(type) {
+      const adsConfig = self.root.settings.ads;
+
+      if (!adsConfig || !adsConfig.formats) return {};
+
+      const formatsList = adsConfig.formats;
+
+      const typeFormats = formatsList.find(formats => {
+        if (formats.type === type) return true;
+        if (typeof formats.type === 'object' && formats.type.includes(type)) return true;
+
+        return false;
+      });
+
+      return typeFormats || formatsList.find(options => options.type === 'default');
+    },
+    getOptions(type) {
+      const formats = self.getFormats(type);
+      return formats.options;
+    },
+    getContentFormats(type) {
+      const formats = self.getFormats(type);
+      return formats.content;
+    },
+    getStickyFormat(type) {
+      const formats = self.getFormats(type);
+      return formats.sticky;
+    },
+  }));
