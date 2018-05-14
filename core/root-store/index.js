32d7d01a68aa5efd1a1cad77e7730f19453ec06d
Luis Herranz <luisherranz@gmail.com>
2018-05-14 17:54:43 +0200

    Add tests for build

--- core/root-store/index.js
+++ core/root-store/index.js
@@ -1,61 +1,8 @@
 /* eslint-disable no-console */
 import { types, onAction } from 'mobx-state-tree';
+import Build from './build';
 
 const dev = process.env.NODE_ENV !== 'production';
-
-const Build = types
-  .model('Build')
-  .props({
-    siteId: types.string,
-    channel: types.enumeration(['amp', 'pwa']),
-    device: types.enumeration(['mobile', 'tablet, desktop']),
-    rendering: types.enumeration(['ssr', 'csr']),
-    packages: types.frozen,
-    isDev: types.optional(types.boolean, false),
-    initialUrl: types.maybe(types.string),
-    perPage: types.number,
-  })
-  .views(self => ({
-    get isAmp() {
-      return self.channel === 'amp';
-    },
-    get isPwa() {
-      return self.channel === 'pwa';
-    },
-    isChannel: channel => self.channel === channel,
-    get isMobile() {
-      return self.device === 'mobile';
-    },
-    get isTablet() {
-      return self.device === 'tablet';
-    },
-    get isDesktop() {
-      return self.device === 'tablet';
-    },
-    isDevice: device => self.device === device,
-    get machine() {
-      return typeof window !== 'undefined' ? 'client' : 'server';
-    },
-    get isServer() {
-      return self.machine === 'server';
-    },
-    get isClient() {
-      return self.machine === 'client';
-    },
-    isMachine: machine => self.machine === machine,
-    get isSsr() {
-      return self.rendering === 'ssr';
-    },
-    get isCsr() {
-      return self.rendering === 'csr';
-    },
-    isRendering: rendering => self.rendering === rendering,
-  }))
-  .actions(self => ({
-    switchToCsr: () => {
-      self.rendering = 'csr';
-    },
-  }));
 
 const Store = types
   .model('Store')
