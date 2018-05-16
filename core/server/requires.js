6174bec2f031f452f8f2e26e4511fdc2669db447
Luis Herranz <luisherranz@gmail.com>
2018-05-16 16:09:49 +0200

    Refactor requires

--- core/server/requires.js
+++ core/server/requires.js
@@ -1,36 +1,10 @@
-/* eslint-disable global-require, import/no-dynamic-require, import/prefer-default-export,
-no-restricted-syntax, no-restricted-globals, no-await-in-loop */
-import { pathExists } from 'fs-extra';
-
-const pathExistsPromise = name =>
-  new Promise((resolve, reject) => {
-    pathExists(`./packages/${name}/src/${process.env.MODE}/index.js`).then(path => {
-      if (path) resolve(true);
-      else reject(new Error(`Module ${name} is not installed.`));
-    });
-  });
-
-export const requireModules = async pkgs => {
-  const pathPromises = pkgs.map(([, name]) => pathExistsPromise(name));
-  await Promise.all(pathPromises);
-  return pkgs.map(([namespace, name]) => {
-    const module = require(`../../packages/${name}/src/${process.env.MODE}/index`);
-
+/* eslint-disable global-require, import/no-dynamic-require */
+export default pkgs =>
+  pkgs.map(([namespace, name]) => {
     try {
-      // Only return serverSaga if it exists.
-      module.serverSagas = require(`../../packages/${name}/src/${
-        process.env.MODE
-      }/sagas/server`).default;
-    } catch (e) {
-      // Not adding serverSaga.
-    }
-    try {
-      // Only return serverFlow if it exists.
-      const serverFlow = require(`../../packages/${name}/src/${process.env.MODE}/flows/server`)
-        .default;
-      return { name, namespace, module: { ...module, serverFlow } };
-    } catch (e) {
+      const module = require(`../../packages/${name}/src/${process.env.MODE}/index`);
       return { name, namespace, module };
+    } catch (error) {
+      throw new Error(`Module ${name} is not installed.`);
     }
   });
-};
