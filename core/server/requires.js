cb0578b32a86f90a36eca04cde3d0f836e936264
Luis Herranz <luisherranz@gmail.com>
2018-05-17 18:29:20 +0200

    Don't use try on requires
    Fixes #83

--- core/server/requires.js
+++ core/server/requires.js
@@ -1,10 +1,19 @@
 /* eslint-disable global-require, import/no-dynamic-require */
-export default pkgs =>
-  pkgs.map(([namespace, name]) => {
-    try {
-      const module = require(`../../packages/${name}/src/${process.env.MODE}/server`);
-      return { name, namespace, module };
-    } catch (error) {
-      throw new Error(`Module ${name} is not installed.`);
-    }
+import { pathExists } from 'fs-extra';
+
+const pathExistsPromise = name =>
+  new Promise((resolve, reject) => {
+    pathExists(`./packages/${name}/src/${process.env.MODE}/server.js`).then(path => {
+      if (path) resolve(true);
+      else reject(new Error(`Module ${name} is not installed.`));
+    });
   });
+
+export default async pkgs => {
+  const pathPromises = pkgs.map(([, name]) => pathExistsPromise(name));
+  await Promise.all(pathPromises);
+  return pkgs.map(([namespace, name]) => {
+    const module = require(`../../packages/${name}/src/${process.env.MODE}/server`);
+    return { name, namespace, module };
+  });
+}
