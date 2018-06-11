2df967bafef62fab8f8a64d29b588acc675f232d
Luis Herranz <luisherranz@gmail.com>
2018-06-11 20:04:57 +0200

    Improvements to SW
    Fixes #93, #91 and #90

--- core/sw/index.js
+++ core/sw/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-undef */
+/* eslint-disable no-undef, no-restricted-globals */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
 const serviceWorkerOptionAssets = serviceWorkerOption.assets || [];
@@ -10,15 +10,75 @@ const entry = `${dynamicUrl}?siteId=${siteId}&type=${type}&id=${id}&dev=false&st
 )}&dynamicUrl=${encodeURIComponent(
   dynamicUrl,
 )}&env=prod&perPage=${perPage}&device=mobile&initialUrl=${encodeURIComponent(initialUrl)}`;
+const manifest = `${dynamicUrl}dynamic/wordcamp-theme/${siteId}/manifest.json`;
 
 workbox.precaching.precacheAndRoute([
   ...serviceWorkerOptionAssets
     .filter(asset => !/^\/bootstrap\.js$/.test(asset))
     .map(asset => `${staticUrl}static${asset}`),
   ...additionalFiles,
-  { url: initialUrl, revision: new Date().getTime() },
-  { url: entry, revision: new Date().getTime() },
 ]);
+
+const frontityStrategy = workbox.strategies.networkFirst({
+  cacheName: 'frontity',
+  networkTimeoutSeconds: 30,
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    }),
+  ],
+});
+
+const wordpressStrategy = workbox.strategies.networkFirst({
+  cacheName: 'wordpress',
+  networkTimeoutSeconds: 30,
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    }),
+    new workbox.cacheableResponse.Plugin({
+      statuses: [200],
+    }),
+  ],
+});
+
+const manifestStrategy = workbox.strategies.networkFirst({
+  cacheName: 'manifest',
+  networkTimeoutSeconds: 30,
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxEntries: 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    }),
+    new workbox.cacheableResponse.Plugin({
+      statuses: [200],
+    }),
+  ],
+});
+
+self.addEventListener('install', event => {
+  frontityStrategy.makeRequest({ event, request: entry });
+  wordpressStrategy.makeRequest({ event, request: initialUrl });
+  manifestStrategy.makeRequest({ event, request: manifest });
+});
+
+workbox.routing.registerRoute(
+  ({ url }) => new RegExp(`siteId=${siteId}`).test(url.href),
+  frontityStrategy,
+);
+
+workbox.routing.registerRoute(
+  ({ url }) => new RegExp(`${siteId}/manifest.json$`).test(url.href),
+  manifestStrategy,
+);
+
+workbox.routing.registerRoute(
+  ({ event }) => event.request.mode === 'navigate',
+  args =>
+  wordpressStrategy.handle(args).then(response => (!response ? caches.match('/') : response)),
+);
 
 workbox.routing.registerRoute(
   ({ url }) => /\.(?:png|gif|jpg|jpeg|svg)/.test(url.href),
