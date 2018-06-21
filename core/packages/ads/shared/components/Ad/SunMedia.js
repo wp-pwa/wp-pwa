f60c9f3bdaef6d4a80ab3b779923560c42acabb6
Eduardo Campaña <asrielo@gmail.com>
2018-06-21 13:00:50 +0200

    Adds support for amp (even if we can't use it)
    https://github.com/wp-pwa/wp-pwa-plugin/issues/69

--- core/packages/ads/shared/components/Ad/SunMedia.js
+++ core/packages/ads/shared/components/Ad/SunMedia.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 
 class SunMedia extends Component {
+  static propTypes = {
+    src: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired,
+    isAmp: PropTypes.bool.isRequired,
+    cid: PropTypes.string,
+  };
+
+  static defaultProps = {
+    cid: null,
+  };
+
   componentDidMount() {
+    if (this.props.isAmp) return;
+
     const script = document.createElement('script');
     script.async = true;
     script.type = 'application/javascript';
@@ -11,9 +25,27 @@ class SunMedia extends Component {
   }
 
   render() {
-    return this.props.active ? (
+    const { active, isAmp, cid } = this.props;
+
+    // This won't work for now, as we are filtering out sunmedia ads on AMP,
+    // but we can try again later if we have some way to get the `data-cid`.
+    if (isAmp) {
+      return (
+        <Fragment>
+          <Helmet>
+            <script
+              async=""
+              custom-element="amp-ad"
+              src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"
+            />
+          </Helmet>
+          <amp-ad type="sunmedia" layout="fill" data-cid={cid} data-cskp={1} data-crst={1} />
+        </Fragment>
+      );
+    }
+
+    return active ? (
       <div
-        id="sunmedia-contaier"
         ref={node => {
           this.ref = node;
         }}
@@ -21,10 +53,5 @@ class SunMedia extends Component {
     ) : null;
   }
 }
-
-SunMedia.propTypes = {
-  src: PropTypes.string.isRequired,
-  active: PropTypes.bool.isRequired,
-};
 
 export default SunMedia;
