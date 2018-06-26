18d13aa861dd17844e4eac7d14c8884824f06e0b
David Arenas <darensr@gmail.com>
2018-06-26 18:50:02 +0200

    refactor(store): simplify code

--- core/packages/analytics/pwa/index.js
+++ core/packages/analytics/pwa/index.js
@@ -1,6 +1,5 @@
 import components from './components';
-import clientSagas from './sagas/client';
 import Store from '../shared/stores';
 
 export default components;
-export { clientSagas, Store };
+export { Store };
--- core/packages/analytics/shared/stores/index.js
+++ core/packages/analytics/shared/stores/index.js
@@ -1,59 +1,29 @@
-import { types, getEnv } from 'mobx-state-tree';
-import { dep } from 'worona-deps';
+import { types } from 'mobx-state-tree';
 
-const mapCustomDimensions = (self, action) => {
-  const { customDimensions } = self;
-  const { entities } = action;
-
-  const entityTypes = Object.keys(entities);
-
-  entityTypes.forEach(type => {
-    const entityIds = Object.keys(entities[type]);
-
-    entityIds.forEach(id => {
-      if (entities[type][id].custom_analytics) {
-        if (!customDimensions.get(entities[type][id].type)) {
-          customDimensions.set(entities[type][id].type, {});
-        }
-
-        if (!customDimensions.get(entities[type][id].type).get(id)) {
-          customDimensions
-            .get(entities[type][id].type, {})
-            .set(id, entities[type][id].custom_analytics);
-        }
-      }
-    });
-  });
-};
+const getKey = ({ type, id }) => `${type}_${id}`;
 
 const Analytics = types
   .model('Analytics')
   .props({
-    customDimensions: types.optional(types.map(types.map(types.frozen)), {}),
+    customDimensionMap: types.optional(types.map(types.frozen), {}),
   })
   .actions(self => ({
-    [dep('connection', 'actionTypes', 'ENTITY_SUCCEED')](action) {
-      mapCustomDimensions(self, action);
-    },
-    [dep('connection', 'actionTypes', 'LIST_SUCCEED')](action) {
-      mapCustomDimensions(self, action);
+    addCustomDimensions(rawEntities) {
+      Object.keys(rawEntities).forEach(type => {
+        Object.keys(rawEntities[type]).forEach(id => {
+          const key = getKey({ type, id });
+          const { custom_analytics: customDimensions } = rawEntities[type][id];
+
+          if (!self.customDimensionMap.has(key)) {
+            self.customDimensionMap.set(key, customDimensions);
+          }
+        });
+      });
     },
   }))
   .views(self => ({
-    getCustomDimensions({ type, id }) {
-      if (type && id) {
-        const typeList = self.customDimensions.get(type);
-
-        if (typeList) {
-          const dimensions = typeList.get(id.toString());
-
-          if (dimensions) {
-            return dimensions;
-          }
-        }
-      }
-
-      return null;
+    customDimensions(entity) {
+      return self.customDimensionMap.get(getKey(entity)) || null;
     },
   }));
 
