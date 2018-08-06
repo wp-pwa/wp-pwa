96dbb2c58d2bf6a954c8c1a75bd4ca6665df6248
Eduardo Campaña <asrielo@gmail.com>
2018-08-06 12:10:25 +0200

    fix(ads): removes sunmedia ads in AMP
    fixes https://github.com/frontity/frontity/issues/38

--- core/packages/ads/shared/components/Ad/SunMedia.js
+++ core/packages/ads/shared/components/Ad/SunMedia.js
@@ -1,25 +1,16 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
-import { Helmet } from 'react-helmet';
 
 class SunMedia extends Component {
   static propTypes = {
     src: PropTypes.string.isRequired,
     active: PropTypes.bool.isRequired,
-    isAmp: PropTypes.bool.isRequired,
-    cid: PropTypes.string,
     width: PropTypes.number.isRequired,
     height: PropTypes.number.isRequired,
   };
 
-  static defaultProps = {
-    cid: null,
-  };
-
   componentDidMount() {
-    if (this.props.isAmp) return;
-
     const script = window.document.createElement('script');
     script.async = true;
     script.type = 'application/javascript';
@@ -28,30 +19,7 @@ class SunMedia extends Component {
   }
 
   render() {
-    const { active, isAmp, cid, width, height } = this.props;
-
-    // This won't work for now, as we are filtering out sunmedia ads on AMP,
-    // but we can try again later if we have some way to get the `data-cid`.
-    if (isAmp) {
-      return (
-        <Fragment>
-          <Helmet>
-            <script
-              async=""
-              custom-element="amp-ad"
-              src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"
-            />
-          </Helmet>
-          <amp-ad
-            type="sunmedia"
-            layout="fill"
-            data-cid={cid}
-            data-cskp={1}
-            data-crst={1}
-          />
-        </Fragment>
-      );
-    }
+    const { active, width, height } = this.props;
 
     return active ? (
       <Container
--- core/packages/ads/shared/components/index.js
+++ core/packages/ads/shared/components/index.js
@@ -5,12 +5,18 @@ import { Fill } from 'react-slot-fill';
 import Ad from './Ad';
 import Sticky from './Sticky';
 
-const Ads = ({ fills }) =>
-  fills.map(({ name, isSticky, ...adProps }) => (
-    <Fill key={name} name={name}>
-      {isSticky ? <Sticky format={adProps} slotName={name} /> : <Ad {...adProps} slotName={name} />}
-    </Fill>
-  ));
+const Ads = ({ fills, isAmp }) =>
+  (isAmp ? fills.filter(item => item.type !== 'sunmedia') : fills).map(
+    ({ name, isSticky, ...adProps }) => (
+      <Fill key={name} name={name}>
+        {isSticky ? (
+          <Sticky format={adProps} slotName={name} />
+        ) : (
+          <Ad {...adProps} slotName={name} />
+        )}
+      </Fill>
+    ),
+  );
 
 Ads.propTypes = {
   fills: PropTypes.arrayOf(
@@ -18,12 +24,14 @@ Ads.propTypes = {
       name: PropTypes.string.isRequired,
     }),
   ),
+  isAmp: PropTypes.bool.isRequired,
 };
 
-export default inject(({ stores: { settings } }) => {
+export default inject(({ stores: { build, settings } }) => {
   const ads = settings.theme.ads || {};
 
   return {
+    isAmp: build.isAmp,
     fills: ads.fills || [],
     adsAreLazy: ads.areLazy,
   };
