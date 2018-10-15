0d62902b59c01a677b672baa11218fd4d521c102
David Arenas <darensr@gmail.com>
2018-10-15 18:11:30 +0200

    fix(script): add script tag in runtime

--- core/packages/ads/shared/components/Script/index.js
+++ core/packages/ads/shared/components/Script/index.js
@@ -1,30 +1,55 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { inject } from 'mobx-react';
 
-const Script = ({ func, args }) => {
-  const code = `(function(){${func} f(${args
-    .map(JSON.stringify)
-    .join(',')})})();`;
+class Script extends React.Component {
+  constructor(props) {
+    super(props);
+    this.isSsr = props.isSsr;
+    this.node = React.createRef();
+  }
 
-  const scriptTag = 'script';
-  return (
-    <span
-      suppressHydrationWarning
-      dangerouslySetInnerHTML={{
-        __html: `<${scriptTag}>${code}</${scriptTag}>`,
-      }}
-    />
-  );
-};
+  componentDidMount() {
+    if (!this.isSsr) {
+      const script = window.document.createElement('script');
+      script.textContent = this.textContent();
+      this.node.current.appendChild(script);
+    }
+  }
+
+  textContent = () => {
+    const { func, args } = this.props;
+    return `(function(){${func} func(${args
+      .map(JSON.stringify)
+      .join(',')})})();`;
+  };
+
+  render() {
+    const tag = 'script';
+    return this.isSsr ? (
+      <span
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: `<${tag}>${this.textContent()}</${tag}>`,
+        }}
+      />
+    ) : (
+      <span ref={this.node} />
+    );
+  }
+}
 
 Script.propTypes = {
   func: PropTypes.string.isRequired,
   args: PropTypes.arrayOf(PropTypes.any),
+  isSsr: PropTypes.bool.isRequired,
 };
 
 Script.defaultProps = {
   args: [],
 };
 
-export default Script;
+export default inject(({ stores }) => ({
+  isSsr: stores.build.isSsr,
+}))(Script);
