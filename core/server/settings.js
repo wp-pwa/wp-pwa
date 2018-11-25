7335f0d68bcc03fbbf5e6691ea91f15a2e8c6693
Luis Herranz <luisherranz@gmail.com>
2018-11-25 20:49:43 +0100

    fix(settings): switch to production database

--- core/server/settings.js
+++ core/server/settings.js
@@ -3,14 +3,11 @@ import { Lokka } from 'lokka';
 import { Transport } from 'lokka-transport-http';
 
 const client = new Lokka({
-  transport: new Transport(
-    'https://api.graph.cool/simple/v1/frontity-v1-stage',
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
-      },
+  transport: new Transport('https://api.graph.cool/simple/v1/frontity-v1', {
+    headers: {
+      Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
     },
-  ),
+  }),
 });
 
 const packageSchema = new schema.Entity(
