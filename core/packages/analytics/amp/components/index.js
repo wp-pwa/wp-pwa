4b3e853329c263f70571a29b5e4ed6dee4754cbc
Eduardo Campaña <asrielo@gmail.com>
2019-02-07 14:35:28 +0100

    fix(analytics): fixes proptypes warning
    fixes https://github.com/frontity/frontity/issues/170

--- core/packages/analytics/amp/components/index.js
+++ core/packages/analytics/amp/components/index.js
@@ -15,31 +15,31 @@ const Analytics = ({
   gtmVars,
   comScoreIds,
 }) => (
-  <Fragment>
-    {gaIds.map(gaId => {
-      const { sendPageViews, sendEvents } = gaTrackingOptions(gaId);
-      return (
-        <GoogleAnalytics
-          key={gaId}
-          id={gaId}
-          pageView={sendPageViews && gaPageView}
-          vars={gaVars}
-          triggers={sendEvents && gaTriggers}
-        />
-      );
-    })}
-    {gtmIds.map(gtmId => (
-      <GoogleTagManager key={gtmId} id={gtmId} vars={gtmVars} />
-    ))}
-    {comScoreIds.map(comScoreId => (
-      <ComScore key={comScoreId} id={comScoreId} />
-    ))}
-  </Fragment>
-);
+    <Fragment>
+      {gaIds.map(gaId => {
+        const { sendPageViews, sendEvents } = gaTrackingOptions(gaId);
+        return (
+          <GoogleAnalytics
+            key={gaId}
+            id={gaId}
+            pageView={sendPageViews && gaPageView}
+            vars={gaVars}
+            triggers={sendEvents && gaTriggers}
+          />
+        );
+      })}
+      {gtmIds.map(gtmId => (
+        <GoogleTagManager key={gtmId} id={gtmId} vars={gtmVars} />
+      ))}
+      {comScoreIds.map(comScoreId => (
+        <ComScore key={comScoreId} id={comScoreId} />
+      ))}
+    </Fragment>
+  );
 
 Analytics.propTypes = {
   gaIds: PropTypes.arrayOf(PropTypes.string).isRequired,
-  gaTrackingOptions: PropTypes.shape({}).isRequired,
+  gaTrackingOptions: PropTypes.func,
   gaPageView: PropTypes.shape({}).isRequired,
   gaVars: PropTypes.shape({}),
   gaTriggers: PropTypes.shape({}),
@@ -54,6 +54,7 @@ Analytics.defaultProps = {
   gaVars: {},
   gaTriggers: {},
   gtmVars: {},
+  gaTrackingOptions: null,
 };
 
 export default inject(({ stores: { analytics } }) => {
@@ -64,7 +65,7 @@ export default inject(({ stores: { analytics } }) => {
     gaIds,
     gaTrackingOptions: gaIds.length
       ? analytics.googleAnalytics.trackingOptions
-      : {},
+      : null,
     gaPageView: gaIds.length ? analytics.googleAnalytics.pageViewAmp : {},
     gaVars: gaIds.length ? analytics.googleAnalytics.ampVars : {},
     gaTriggers: gaIds.length ? analytics.googleAnalytics.ampTriggers : {},
