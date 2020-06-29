bd2f854b3fefc6cc0cbf6b82fc20edc3e8ccb509
Luis Herranz <luisherranz@gmail.com>
2020-06-29 13:16:05 +0200

    fix: update settings to use static JSON files

--- core/server/settings.js
+++ core/server/settings.js
@@ -1,4 +1,14 @@
 import { normalize, schema } from 'normalizr';
+// import { Lokka } from 'lokka';
+// import { Transport } from 'lokka-transport-http';
+
+// const client = new Lokka({
+//   transport: new Transport('https://api.graph.cool/simple/v1/frontity-v1', {
+//     headers: {
+//       Authorization: `Bearer ${process.env.GRAPHQL_TOKEN}`,
+//     },
+//   }),
+// });
 
 const packageSchema = new schema.Entity(
   'packages',
@@ -18,8 +28,32 @@ const settingsSchema = [settingSchema];
 // Fetch settings from the backend.
 export default async ({ siteId }) => {
   try {
-    // eslint-disable-next-line global-require, import/no-dynamic-require
-    const allSettings = require(`../../sites/${siteId}`);
+    const {
+      data: { allSettings },
+      // eslint-disable-next-line global-require, import/no-dynamic-require
+    } = require(`../../sites/${siteId}`);
+    // const { allSettings } = await client.query(`
+    //   {
+    //     allSettings(filter: {
+    //       site: {
+    //         siteId: "${siteId}"
+    //       }
+    //       active: true
+    //     }) {
+    //       site {
+    //         url
+    //         users {
+    //           id
+    //         }
+    //       }
+    //       package {
+    //         name
+    //         namespace
+    //       }
+    //       data
+    //     }
+    //   }
+    // `);
     const { entities } = normalize(allSettings, settingsSchema);
     const settings = Object.entries(entities.settings).reduce(
       (obj, [namespace, setting]) => {
